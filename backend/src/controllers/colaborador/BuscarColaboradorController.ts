b9916cbb24ca463dec097fa15c2cf4257136b000
Denivan Dias <denivanbass@gmail.com>
2026-08-26 02:11:30 -0300

    alteração de status

--- backend/src/controllers/colaborador/BuscarColaboradorController.ts
+++ backend/src/controllers/colaborador/BuscarColaboradorController.ts
@@ -11,7 +11,7 @@ class BuscarColaboradorController {
 
         const colaboradores = await buscarColaboradorService.execute()
 
-        return res.json(colaboradores)
+        return res.json(colaboradores).status(200)
     }
 }
 export  { BuscarColaboradorController }

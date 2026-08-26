d8f35acad78186cf553a9609a26b6150a4f075bf
Denivan Dias <denivanbass@gmail.com>
2026-08-26 20:38:05 -0300

    refactor: remover adapter mariadb e ajustar prisma client singleton

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-// Padrão Singleton para evitar estourar o limite de conexões em produção
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  connectionLimit: 5,
+});
+const prisma = new PrismaClient({ adapter });
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new (PrismaClient as any)();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export { prisma };

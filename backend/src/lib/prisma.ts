b5f699667dbe42ded2756b4a4711d0e921af888a
Denivan Dias <denivanbass@gmail.com>
2026-08-27 23:06:27 -0300

    refactor: remover adapter mariadb e ajustar prisma client singleton

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -3,12 +3,15 @@ import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "../generated/prisma/client.js";
 
 const adapter = new PrismaMariaDb({
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST!,
   port: Number(process.env.DATABASE_PORT || 3306),
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  connectionLimit: 5,
+  user: process.env.DATABASE_USER!,
+  password: process.env.DATABASE_PASSWORD!,
+  database: process.env.DATABASE_NAME!,
+
+  connectionLimit: 1,
+  connectTimeout: 30000,
+  acquireTimeout: 30000,
 });
 
 export const prisma = new PrismaClient({
